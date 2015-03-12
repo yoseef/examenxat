d029bae21491d5dcf663e48125017b2af0c1c775
yoseef <yoseef@gmail.com>
2015-03-12 15:24:29 +0100

    db to mongoLab

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/llibre", function() {
+mongoose.connect("mongodb://ysf:ysf123@ds033059.mongolab.com:33059/llibre", function() {
     console.log('Connectat a mongodb');
 });
 module.exports = mongoose;
