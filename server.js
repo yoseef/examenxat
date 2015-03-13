b8d6b805cf9d405e5a0360fc286bb3a15a2b1cb2
root <insyosef@gmail.com>
2015-03-13 15:35:55 +0100

    server port

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var port = process.env.PORT || 8888;
 var app = express();
 app.use(bodyParser.json());
 
 app.use(require('./controllers/static'));
 app.use('/api/llibres',require('./controllers/api/llibres/llibres'));
 
-app.listen(8888, function () {
-    console.log('Server listening on  http://localhost:8888');
+app.listen(port, function () {
+    console.log('Server listening on  http://localhost:' + port);
 });
